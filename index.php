95a8f71629d4a863244df142d10408623b896aef
FernandoAntonioAzevedo <beratechsolutions@gmail.com>
2025-04-15 16:39:50 -0300

    inserindo estrutura ao projeto como cdns e biblioteca bootstrap

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+  include_once("config/url.php");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">    
+    <!-- BootStrap -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.3/css/bootstrap.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" 
+    integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" 
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <title>Agenda de contatos</title>
+</head>
+<body>
+    
+</body>
+</html>
