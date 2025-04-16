2532d6bc2e01abe2eebb9c7ab5ee610a0c1b9db7
FernandoAntonioAzevedo <beratechsolutions@gmail.com>
2025-04-16 14:28:16 -0300

    criando página inicial da agenda e apontando para create.php

--- config/process.php
+++ config/process.php
@@ -4,3 +4,13 @@
 
  include_once("connection.php");
  include_once("url.php");
+
+ $contacts = [];
+ 
+ $query = "SELECT * FROM contacts";
+
+ $stmt = $conn->prepare("$query");
+
+ $stmt->execute();
+
+ $contacts = $stmt->fetchAll();
--- index.php
+++ index.php
@@ -1,8 +1,17 @@
 <?php
     include_once("templates/header.php");
 ?>    
-    <h1>Testando aplicação</h1>
-    <i class="fas fa-eye"></i>
+   <div class="container">
+     <?php if(isset($printMsg) && $printMsg != '') : ?>
+        <p id="msg"><?= $printMsg ?></p>
+     <?php endif; ?>
+     <h1 id="main-title">Minha agenda</h1>
+     <?php if(count($contacts) > 0): ?>
+        <p>Tem contatos</p>
+     <?php else: ?>
+        <p id="empty-list-text">Ainda não há contatos na sua agenda, <a href="<? $BASE_URL ?>create.php">clique aqui para adicionar</a>.</p>
+     <?php endif; ?>   
+   </div>
 
 <?php 
     include_once("templates/footer.php");
